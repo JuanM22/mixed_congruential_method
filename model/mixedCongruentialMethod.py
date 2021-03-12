4b2244390f1425eef7974f45c4e654e82766d7f6
Juan <jmartinezsb2020@gmail.com>
2021-03-12 12:08:52 -0500

    Period finder added to generateNumbers function

--- model/mixedCongruentialMethod.py
+++ model/mixedCongruentialMethod.py
@@ -1,17 +1,43 @@
 class MixedCongruentialMethod:
 
     def __init__(self, X0, a, c, m):
-        self.intValues = []
+        self.period = []
         self.doubleValues = []
         self.Xn = X0
         self.a = a
         self.c = c
         self.m = m
 
-
     def generateNumbers(self):
-        for _ in range(0,self.m):
+        periodFounded = False
+        for i in range(0, self.m + 1):
             number = ((self.a * self.Xn) + self.c) % self.m
-            self.intValues.append(number)  
-            self.doubleValues.append("{:.4f}".format(round(number/self.m, 4)))
+            if(not(periodFounded)):
+                doubleNumber = (number/self.m)
+                if(doubleNumber in self.doubleValues):
+                    self.period.append(self.doubleValues.index(
+                        doubleNumber))  # Inicio de periodo
+                    self.period.append(i)  # Fin de periodo
+                    periodFounded = True
+            self.doubleValues.append(number/self.m)
             self.Xn = number
+
+    def roundNumbers(self):
+        for i, number in enumerate(self.doubleValues):
+            self.doubleValues[i] = round(number,4)
+
+
+# mixedCongruentialMethod = MixedCongruentialMethod(X0, a, c, m)
+# mixedCongruentialMethod.generateNumbers()
+# mixedCongruentialMethod.roundNumbers()
+# print(mixedCongruentialMethod.doubleValues)
+# print(mixedCongruentialMethod.period)
+# print('')
+
+# start = mixedCongruentialMethod.period[0]
+# end = mixedCongruentialMethod.period[1]
+
+# print(mixedCongruentialMethod.period)
+# print('Periodo -> ', (end - start))
+# for i in range(start, end):
+#     print(mixedCongruentialMethod.doubleValues[i])
